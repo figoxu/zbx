563964c4fc948b90d5b983d4c1832b08ebba9104
figoxu <xujh945@qq.com>
2018-07-23 17:29:50 +0800

    增加语音通知的报错机制

--- znotify/sms.go
+++ znotify/sms.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"crypto/md5"
 	"strconv"
+	"github.com/quexer/utee"
+	"github.com/figoxu/Figo"
 )
 
 /**
@@ -18,8 +20,9 @@ import (
  * mobile: 手机号码
  * content: 短信内容
  */
-func SendVoice(appId,appKey,mobile,content string){
-	md5Str :=func(s string) string {
+func SendVoice(appId, appKey, mobile, content string) error {
+	defer Figo.Catch()
+	md5Str := func(s string) string {
 		h := md5.New()
 		h.Write([]byte(s))
 		return hex.EncodeToString(h.Sum(nil))
@@ -33,12 +36,15 @@ func SendVoice(appId,appKey,mobile,content string){
 	v.Set("time", _now)
 	body := ioutil.NopCloser(strings.NewReader(v.Encode())) //把form数据编下码
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", "http://api.vm.ihuyi.com/webservice/voice.php?method=Submit&format=json", body)
+	req, err := http.NewRequest("POST", "http://api.vm.ihuyi.com/webservice/voice.php?method=Submit&format=json", body)
+	utee.Chk(err)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 	//fmt.Printf("%+v\n", req) //看下发送的结构
 	resp, err := client.Do(req) //发送
-	defer resp.Body.Close()     //一定要关闭resp.Body
-	data, _ := ioutil.ReadAll(resp.Body)
+	utee.Chk(err)
+	defer resp.Body.Close() //一定要关闭resp.Body
+	data, err := ioutil.ReadAll(resp.Body)
+	utee.Chk(err)
 	fmt.Println(string(data), err)
+	return err
 }
-
